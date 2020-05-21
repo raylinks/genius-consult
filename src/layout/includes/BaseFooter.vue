72316de2cf5fd50f9da204025b08ee343f11ddc5
rilwan <olawuyirilwan98@gmail.com>
2020-05-22 00:43:13 +0100

    start in packae js

--- src/layout/includes/BaseFooter.vue
+++ src/layout/includes/BaseFooter.vue
@@ -1,6 +1,6 @@
 <template>
     <footer class="h-16 absolute bottom-0 flex justify-center items-center font-semibold w-full text-center">
-        &copy; 2020 Genius Consult Nigeria. All Rights Reserved
+        &copy; 2020 Genius Consult Inc Nigeria. All Rights Reserved
     </footer>
 </template>
 
