5bc0b5a35b6f202902273c7fde088a1e351a5a38
unknown <luis@patricia.com.ng>
2020-06-01 16:40:36 +0100

    history problem

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 var express = require('express');
+var history = require('connect-history-api-fallback');
 var path = require('path');
 var serveStatic = require('serve-static');
 const app = express();
-app.use(serveStatic(path.join(__dirname, 'dist')));
+app.use(history())
+app.use(serveStatic(path.join(__dirname, '/dist')))
 var port = process.env.PORT || 5000;
 app.listen(port);
 console.log('server started ' + port);
