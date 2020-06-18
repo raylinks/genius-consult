8f40bd3ac54d1d9fb836ee3e7e20cc32c3c55e39
unknown <luis@patricia.com.ng>
2020-06-18 23:45:29 +0100

    adds footer

--- src/layout/includes/BaseFooter.vue
+++ src/layout/includes/BaseFooter.vue
@@ -143,6 +143,31 @@
               >
             </li>
           </ul>
+          <br />
+          <form action="" class="w-full max-w-lg">
+            <div class="flex flex-wrap -mx-3 mb-6">
+              <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
+                <label
+                  class="block  tracking-wide text-gray-700 text-xs mb-2"
+                  for="grid-first-name"
+                >
+                  NewsLetter Signup
+                </label>
+                <input
+                  class="appearance-none block w-400 bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+                  id=""
+                  type="text"
+                  placeholder="Email"
+                />
+                <button
+                  class="flex-shrink-0 bg-teal-500 hover:bg-teal-700 border-teal-500 hover:border-teal-700 text-sm border-4 text-black py-1 px-2 rounded"
+                  type="button"
+                >
+                  Sign Up
+                </button>
+              </div>
+            </div>
+          </form>
         </div>
       </div>
     </div>
